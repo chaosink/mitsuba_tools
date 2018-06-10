e26570a347b896256dfe2a5653e7336cd8a425cb
linhsu0723 <linhsu0723@qq.com>
2018-06-10 21:31:59 +0800

    twosided to onesided

---
+++ twos2ones/twos2ones.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from lxml import etree as et
+# import xml.etree.ElementTree as et
+import sys
+
+if len(sys.argv) < 3:
+	print("Usage: python3 twos2ones.py input_xml output_xml")
+	sys.exit()
+
+input_xml = sys.argv[1]
+output_xml = sys.argv[2]
+
+tree = et.parse(input_xml)
+root = tree.getroot()
+
+twosided_bsdfs = []
+for bsdf in root.iter('bsdf'):
+	if bsdf.get('type') == 'twosided':
+		twosided_bsdfs.append(bsdf)
+
+for bsdf in twosided_bsdfs:
+	bsdf[0].set('id', str(bsdf.get('id')))
+	p = bsdf.getparent()
+	p.insert(0, bsdf[0])
+	p.remove(bsdf)
+
+tree.write(output_xml, pretty_print=True)
+sys.exit()
